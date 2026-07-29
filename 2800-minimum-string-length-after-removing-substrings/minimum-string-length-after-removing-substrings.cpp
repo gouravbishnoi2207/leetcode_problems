8c5e2727023f91d856f506067c6c49408afb4971
Gourav Bishnoi <bishnoigourav655@gmail.com>
2026-07-29 15:36:34 +0530

    Time: 3 ms (48.70%) | Memory: 9.9 MB (99.29%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int i=0;
+        while(s.size() > 0 && i < s.size() - 1){
+            if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
+                s.erase(i,2);
+        
+        if(i>0){
+                i--;
+            }
+            }
+            else{
+                i++;
+            }
+        }
+        return s.size();
+    }
+};
