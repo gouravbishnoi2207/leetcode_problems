39221181ee2db85767746825f30a95e665d9a8a0
Gourav Bishnoi <bishnoigourav655@gmail.com>
2026-08-05 14:14:58 +0530

    Time: 83 ms (16.90%) | Memory: 113.5 MB (10.21%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+       for(int x:nums){
+         mp[x]++; } 
+         for(auto const&x:mp){
+            if(x.second!=1){
+                return true;
+            }
+         }
+         return false;
+    }
+};
