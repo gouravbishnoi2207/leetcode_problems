8fa1638d1c8575fb4fb3cbe3c65b0b39d6c99026
Gourav Bishnoi <bishnoigourav655@gmail.com>
2026-08-24 20:30:20 +0530

    Time: 1 ms (63.29%) | Memory: 12.1 MB (12.29%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char , int> mp;
+        for(int i=0; i<magazine.size(); i++){
+            mp[magazine[i]]++;
+        }
+        for(int j=0; j<ransomNote.size(); j++){
+            if(mp[ransomNote[j]]>0){
+                mp[ransomNote[j]]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
